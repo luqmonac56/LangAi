4685502d3fb7297e618cac12ea536f9f4b202346
luqmonac56 <luqmonac56@gmail.com>
2025-02-22 12:29:08 +0100

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {LanguageContextProvider} from "@/context/LanguageContext";
-
+import { LanguageContextProvider } from "@/context/LanguageContext";
 
 export const metadata: Metadata = {
   title: "Language Ai",
-  description: "to translate text",
+  description: "To translate text",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className="">
+      <head>
+        <meta
+          httpEquiv="origin-trial"
+          content="AsXoU7wJWmSW9vhe7Wn4o4H3BhaSJts7pqFkkjzFTa5PIsJDXn6FB7GFT3Ge3Q7QWlfZbtPE/g7HxHIh0XC6gAIAAAB4eyJvcmlnaW4iOiJodHRwczovL2xhbmctYWktb3BhbC52ZXJjZWwuYXBwOjQ0MyIsImZlYXR1cmUiOiJMYW5ndWFnZURldGVjdGlvbkFQSSIsImV4cGlyeSI6MTc0OTU5OTk5OSwiaXNTdWJkb21haW4iOnRydWV9"
+        />
+      </head>
+      <body>
         <LanguageContextProvider>{children}</LanguageContextProvider>
       </body>
     </html>
